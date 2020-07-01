528dac39bf9519ea24d979bd22c1584a8d2545ec
Kyle Morgan <kmorgan8588@gmail.com>
2020-07-01 12:21:30 -0700

    tests and validates push and pop, sets up for peek

--- Stack/Stack.js
+++ Stack/Stack.js
@@ -3,6 +3,29 @@ const LinkedList = require('../Linked_List/Single/LL');
 
 module.exports = class Stack {
     constructor() {
-        this.size = 0;       
+        this.size = 0;
+        this.stack = new LinkedList();     
+    }
+
+    pop() {
+        const { stack, size } = this;
+        let top = stack.removeFromHead();
+        try {
+            top = top.val;
+        } catch (err) {
+            //do nothing, not a bad error, just have to handle it
+        }
+        if (size) this.size--;
+        return top;
+    }
+
+    push(val) {
+        const { stack } = this;
+        stack.addToHead(val);
+        this.size++;
+    }
+
+    peek() {
+        
     }
 }
--- Stack/Stack.test.js
+++ Stack/Stack.test.js
@@ -8,4 +8,44 @@ beforeEach(() => stack = new Stack());
 test('new stack should have size 0', () => {
     const { size } = stack;
     expect(size).toBe(0);
+});
+
+test('should return null when popping of an empty stack', () => {
+    const result = stack.pop();
+    expect(result).toBe(null);
+});
+
+test('size should not go below 0 when popping off an empty stack', () => {
+    stack.pop();
+    const { size } = stack;
+    expect(size).toBeGreaterThan(-1);
+});
+
+test('size should increase when pushing on the stack', () => {
+    stack.push(1);
+    const { size } = stack;
+    expect(size).toBe(1);
+});
+
+test('should pop off a value that was just pushed', () => {
+    stack.push(15);
+    const result = stack.pop();
+    expect(result).toBe(15);
+    expect(stack.size).toBe(0);
+});
+
+test('should pop all values in reverse order', () => {
+    for (let i = 0; i < 5; i++) {
+        stack.push(i);
+    }
+    const result = [];
+    while(stack.size) {
+        result.push(stack.pop());
+    }
+    expect(result).toEqual(expect.arrayContaining([4,3,2,1,0]));
+});
+
+test('peek should return null in an empty stack', () => {
+    const top = stack.peek();
+    expect(top).toBe(null);
 })
