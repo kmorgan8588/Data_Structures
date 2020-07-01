7a5d1e08a0711525ed80801ca008f7c376d6020f
Kyle Morgan <kmorgan8588@gmail.com>
2020-07-01 11:33:28 -0700

    adds tests and func for adding to list

--- Linked_List/Single/LL.js
+++ Linked_List/Single/LL.js
@@ -1,7 +1,38 @@
 const Node = require('../Node/Node');
 
-module.exports = class LinkedList {
-    constructor() {
+/**
+ * Constructed for the purpose of interfacing as data storage for the Stack data structure
+ * Emphasis being place on quick insertion and removal from the head
+ * Constructor()
+ * val {int}: - provided at instatiation, if no value is included, head and tail are set to null
+ * this.head {node}: - see Node/Node.js for more information, contains the node at the front of the list or null
+ * this.tail {node}: - see Node/Node.js for more information, contains the node at the back of the list or null
+ * 
+ * Methods
+ * addToHead(val) {function}: - replaces the head node and attaches the previous head to the next attribute, if no head exists, the tail is also set to the new Node
+ * removeFromHead() {function}: - removes the head node from the list and moves it to the next node in the list, if there is no next node, the tail is also set to null
+ * 
+ */
 
+module.exports = class LinkedList {
+    constructor(val = null) {
+        if (val) {
+            const node = new Node(val);
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.head = val;
+            this.tail = val;
+        }
+    }
+
+    addToHead(val) {
+        const node = new Node(val);
+        if (this.head) {
+            node.next = this.head;
+        } else {
+            this.tail = node;
+        }
+        this.head = node;
     }
 };
--- Linked_List/Single/SinglyLL.test.js
+++ Linked_List/Single/SinglyLL.test.js
@@ -3,9 +3,32 @@ const List = require('./LL');
 let list;
 
 beforeEach(() => list = new List());
-
+afterAll(() => list = null);
 test('a newly instantiated list exists', () => {
     const { head, tail } = list;
     expect(head).toBe(null);
     expect(tail).toBe(null);
 });
+
+test('a list instatiated with a value should have head and tail set to that value', () => {
+    list = new List(5);
+    const {head, tail} = list;
+    expect(head.val).toBe(5);
+    expect(head.next).toBe(null);
+    expect(tail.val).toBe(5);
+    expect(tail.next).toBe(null);
+});
+
+test('should assign head, tail on an empty list', () => {
+    list.addToHead(42);
+    const {head, tail} = list;
+    expect(head.val).toBe(42);
+    expect(tail.val).toBe(42);
+})
+
+test('should insert a new value at head position', () => {
+    list = new List(42);
+    list.addToHead(15);
+    const { head } = list;
+    expect(head.val).toBe(15);
+})
